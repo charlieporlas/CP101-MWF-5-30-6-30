7451a3a0dcbae3eb81e9e5c2c7efc240e566ee68
charlieporlas <charlieporlas123@gmail.com>
2024-09-25 22:36:43 +0800

    Create Porlas.py

---
+++ Activity 4/Porlas.py
@@ -0,0 +1,12 @@
+#sampleText1 = "My name is {}. I love {}. I love playing {}. {}"
+#sampleText1a = sampleText1.format("Charlie", "Porlas", "Baskitball")
+#print(sampleText1a)
+
+
+#sampleText2 = "My name is {1}. I love {2}. I love playing {0}."
+#sampleText2a = sampleText2.format("Codm", "Charlie", "Porlas", "Baskitball", "Wattermelon")
+#print(sampleText2a)
+
+#sampleText3 = "My name is {name}. I love {him}. I love playing {play}."
+#sampleText3a = sampleText3.format(him="Porlas", play="Baskitball", name="Charlie")
+#print(sampleText3a)
